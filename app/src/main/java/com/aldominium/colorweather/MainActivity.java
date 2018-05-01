1b565bc1b53c33abab4853800e8555c7ee9b1cda
Ruben1793 <ruben.ojed9a3@gmail.com>
2018-04-30 22:56:17 -0500

    Probando UI

--- app/src/main/java/com/aldominium/colorweather/MainActivity.java
+++ app/src/main/java/com/aldominium/colorweather/MainActivity.java
@@ -3,7 +3,10 @@ package com.aldominium.colorweather;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
@@ -11,11 +14,25 @@ public class MainActivity extends Activity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
 
+    @BindView(R.id.iconImageView) ImageView iconImageView;
+    @BindView(R.id.descriptionTextView) TextView descriptionTextView;
+    @BindView(R.id.currentTempTextView) TextView currentTempTextView;
+    @BindView(R.id.highestTempTextView) TextView highestTempTextView;
+    @BindView(R.id.lowestTempTextView) TextView lowestTempTextView;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        
+       // iconImageView;
+        descriptionTextView.setText("Sunny Day");
+        currentTempTextView.setText("19");
+        highestTempTextView.setText("H:25");
+        lowestTempTextView.setText("L:10");
     }
 
     @OnClick(R.id.dailyWeatherTextView)
