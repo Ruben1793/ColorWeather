d4835bdc8fe94667056daba72e678a9ec86f91b5
Ruben1793 <ruben.ojed9a3@gmail.com>
2018-05-01 12:58:55 -0500

    Peticiones HTTP Android
    Se agrego Volley

--- app/src/main/java/com/aldominium/colorweather/MainActivity.java
+++ app/src/main/java/com/aldominium/colorweather/MainActivity.java
@@ -4,8 +4,16 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import butterknife.BindDrawable;
 import butterknife.BindView;
@@ -42,6 +50,27 @@ public class MainActivity extends Activity {
         currentTempTextView.setText(currentWeather.getCurrentTemperature());
         highestTempTextView.setText(currentWeather.getHighestTemperature());
         lowestTempTextView.setText(currentWeather.getLowestTemperature());
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://www.google.com";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d(TAG,"Response is: " + response.substring(0, 500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG,"That didn't work!");
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 
     @OnClick(R.id.dailyWeatherTextView)
