a971832c8138ce8c24e276ca1c5d441a7faf43cc
Ruben1793 <ruben.ojed9a3@gmail.com>
2018-05-01 19:39:59 -0500

    Clase Day
    Se creo la clase Day como un POJO

---
+++ app/src/main/java/com/aldominium/colorweather/Day.java
@@ -0,0 +1,32 @@
+package com.aldominium.colorweather;
+
+public class Day {
+
+    private String dayName;
+    private String weatherDescription;
+    private String rainProbability;
+
+    public String getDayName() {
+        return dayName;
+    }
+
+    public void setDayName(String dayName) {
+        this.dayName = dayName;
+    }
+
+    public String getWeatherDescription() {
+        return weatherDescription;
+    }
+
+    public void setWeatherDescription(String weatherDescription) {
+        this.weatherDescription = weatherDescription;
+    }
+
+    public String getRainProbability() {
+        return rainProbability;
+    }
+
+    public void setRainProbability(String rainProbability) {
+        this.rainProbability = rainProbability;
+    }
+}
