22ecd5695c894d537df4dc2d49cb387c3757a710
Ruben1793 <ruben.ojed9a3@gmail.com>
2018-05-05 11:28:30 -0500

    Creando el Hourly Adapter

---
+++ app/src/main/java/com/aldominium/colorweather/Adapters/HourlyWeatherAdapter.java
@@ -0,0 +1,66 @@
+package com.aldominium.colorweather.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.aldominium.colorweather.Hour;
+import com.aldominium.colorweather.R;
+
+import java.util.ArrayList;
+
+public class HourlyWeatherAdapter extends BaseAdapter {
+
+    Context context;
+    ArrayList<Hour> hours;
+
+    public HourlyWeatherAdapter(Context context, ArrayList<Hour> hours){
+        this.context = context;
+        this.hours = hours;
+    }
+
+    @Override
+    public int getCount() {
+        return hours.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return hours.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0; //We dont use this
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup viewGroup) {
+
+        ViewHolder viewHolder;
+        Hour hour = hours.get(position);
+
+        if (view == null){
+            view = LayoutInflater.from(context).inflate(R.layout.hourly_list_item, viewGroup, false);
+            viewHolder = new ViewHolder();
+            viewHolder.title = view.findViewById(R.id.hourlyTitleTextView);
+            viewHolder.description = view.findViewById(R.id.hourlyDescriptionTextView);
+            view.setTag(viewHolder);
+        }  else{
+           viewHolder = (ViewHolder) view.getTag();
+        }
+
+        viewHolder.title.setText(hour.getTitle());
+        viewHolder.description.setText(hour.getWeatherDescription());
+
+        return view;
+    }
+
+    static  class ViewHolder{
+        TextView title;
+        TextView description;
+    }
+}
--- app/src/main/java/com/aldominium/colorweather/HourlyWeatherActivity.java
+++ app/src/main/java/com/aldominium/colorweather/HourlyWeatherActivity.java
@@ -2,12 +2,38 @@ package com.aldominium.colorweather;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.aldominium.colorweather.Adapters.HourlyWeatherAdapter;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class HourlyWeatherActivity extends Activity {
+
+    @BindView(R.id.hourlyListView) ListView hourlyListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hourly_weather);
+        ButterKnife.bind(this);
+
+        Hour hour = new Hour();
+        hour.setTitle("11:00pm");
+        hour.setWeatherDescription("There is a storm!");
+
+        Hour hour2 = new Hour();
+        hour2.setTitle("12:00pm");
+        hour2.setWeatherDescription("There iss still a storm!");
+
+        ArrayList<Hour> hours = new ArrayList<>();
+        hours.add(hour);
+        hours.add(hour2);
+
+        HourlyWeatherAdapter hourlyWeatherAdapter = new HourlyWeatherAdapter(this, hours);
+        hourlyListView.setAdapter(hourlyWeatherAdapter);
     }
 }
