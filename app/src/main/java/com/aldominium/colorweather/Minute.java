7838416d109c773c5eea10d976f23a770ad6e745
Ruben1793 <ruben.ojed9a3@gmail.com>
2018-05-05 11:32:18 -0500

    Creando la clase Minute

---
+++ app/src/main/java/com/aldominium/colorweather/Minute.java
@@ -0,0 +1,24 @@
+package com.aldominium.colorweather;
+
+public class Minute {
+
+    private String title;
+    private String rainProbability;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRainProbability() {
+        return rainProbability;
+    }
+
+    public void setRainProbability(String rainProbability) {
+        this.rainProbability = rainProbability;
+    }
+}
