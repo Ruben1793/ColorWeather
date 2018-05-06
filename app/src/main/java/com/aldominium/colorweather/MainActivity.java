7c5d093ea2a27b8a1aa4b62b2dc473bf1d7c0c52
Ruben1793 <ruben.ojed9a3@gmail.com>
2018-05-05 23:17:38 -0500

    Clima por Hora y JSON

--- app/src/main/java/com/aldominium/colorweather/MainActivity.java
+++ app/src/main/java/com/aldominium/colorweather/MainActivity.java
@@ -148,4 +148,28 @@ public class MainActivity extends Activity {
         }
         return days;
     }
+
+    public ArrayList<Hour> getHourlyWeaterFromJson(String json) throws JSONException {
+        DateFormat dateFormat = new SimpleDateFormat("HH:mm");
+        ArrayList<Hour> hours = new ArrayList<>();
+        JSONObject jsonObject = new JSONObject(json);
+
+        JSONObject jsonWithHourlyWeather = jsonObject.getJSONObject("hourly");
+        JSONArray jsonWithHourlyWeatherData = jsonWithHourlyWeather.getJSONArray("data");
+
+        for (int i = 0; i < jsonWithHourlyWeatherData.length(); i++) {
+            Hour hour = new Hour();
+            JSONObject jsonWithHourData = jsonWithHourlyWeatherData.getJSONObject(i);
+
+            String title = dateFormat.format(jsonWithHourData.getDouble("time") * 1000);
+            String description = jsonWithHourData.getString("summary");
+
+            hour.setTitle(title);
+            hour.setWeatherDescription(description);
+
+            hours.add(hour);
+        }
+
+        return hours;
+    }
 }
