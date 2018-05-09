fb7714568351974b0fb896c970ed66698a64fa44
Ruben1793 <ruben.ojed9a3@gmail.com>
2018-05-08 19:39:19 -0500

    Get Your Location

--- app/src/main/java/com/aldominium/colorweather/MainActivity.java
+++ app/src/main/java/com/aldominium/colorweather/MainActivity.java
@@ -1,9 +1,16 @@
 package com.aldominium.colorweather;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -15,6 +22,9 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -30,38 +40,51 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     public static final String TAG = MainActivity.class.getSimpleName();
     public static final String DATA = "data";
-    public static final String CURRENTLY = "currently";
     public static final String SUMMARY = "summary";
-    public static final String TIMEZONE = "timezone";
-    public static final String HOURLY = "hourly";
-    public static final String TIME = "time";
-    public static final String DAILY = "daily";
-    public static final String PRECIP_PROBABILITY = "precipProbability";
+    public static final String ICON = "icon";
     public static final String TEMPERATURE = "temperature";
     public static final String TEMPERATURE_MAX = "temperatureMax";
     public static final String TEMPERATURE_MIN = "temperatureMin";
+    public static final String CURRENTLY = "currently";
+    public static final String DAILY = "daily";
+    public static final String PRECIP_PROBABILITY = "precipProbability";
+    public static final String HOURLY = "hourly";
+    public static final String TIME = "time";
     public static final String MINUTELY = "minutely";
-    public static final String ICON = "icon";
+    public static final String TIMEZONE = "timezone";
     public static final String DAYS_ARRAY_LIST = "days";
     public static final String HOURS_ARRAY_LIST = "hours";
-    public static final String MINUTES_ARRAY_LIST = "minutes";
-
-    @BindView(R.id.iconImageView) ImageView iconImageView;
-    @BindView(R.id.descriptionTextView) TextView descriptionTextView;
-    @BindView(R.id.currentTempTextView) TextView currentTempTextView;
-    @BindView(R.id.highestTempTextView) TextView highestTempTextView;
-    @BindView(R.id.lowestTempTextView) TextView lowestTempTextView;
-
-    @BindDrawable(R.drawable.clear_night) Drawable clearNight;
+    public static final String MINUTES_ARRAY_LIST = "minutes_array_list";
+    private static final int PERMISSION_ACCESS_FINE_LOCATION = 1;
 
     ArrayList<Day> days;
     ArrayList<Hour> hours;
     ArrayList<Minute> minutes;
 
+    //@BindView(R.id.dailyWeatherTextView) TextView dailyWeatherTextView;
+    @BindView(R.id.iconImageView)
+    ImageView iconImageView;
+    @BindView(R.id.descriptionTextView)
+    TextView descriptionTextView;
+    @BindView(R.id.currentTempTextView)
+    TextView currentTempTextView;
+    @BindView(R.id.highestTempTextView)
+    TextView highestTempTextView;
+    @BindView(R.id.lowestTempTextView)
+    TextView lowestTempTextView;
+
+    @BindDrawable(R.drawable.clear_night)
+    Drawable clearNight;
+
+    //String url = "https://api.darksky.net/forecast/797573bdd40bc15d7f0536c8b663d042/37.8267,-122.4233?units=si";
+
+    GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
     private String mLatitude = "37.8267";
     private String mLongitude = "-122.4233";
 
@@ -71,6 +94,96 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSION_ACCESS_FINE_LOCATION);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode){
+            case PERMISSION_ACCESS_FINE_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getMyLocation();
+                } else {
+                    /*ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_COARSE_LOCATION},
+                            PERMISSION_ACCESS_COARSE_LOCATION);*/
+                    Toast.makeText(this, "Need your location!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+
+    }
+
+    @Override
+    protected void onStop() {
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        getMyLocation();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Toast.makeText(this, "Connection Lost", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, "Fallo la conexion, " + connectionResult.getErrorMessage(),Toast.LENGTH_SHORT).show();
+    }
+
+
+    @OnClick(R.id.dailyWeatherTextView)
+    public void DailyWeatherClick() {
+        Intent dailyActivityIntent = new Intent(MainActivity.this, DailyWeatherActivity.class);
+        dailyActivityIntent.putParcelableArrayListExtra(DAYS_ARRAY_LIST, days);
+        startActivity(dailyActivityIntent);
+    }
+
+    @OnClick(R.id.hourlyWeatherTextView)
+    public void HourlyWeatherClick() {
+        Intent hourlyActivityIntent = new Intent(MainActivity.this, HourlyWeatherActivity.class);
+        hourlyActivityIntent.putParcelableArrayListExtra(HOURS_ARRAY_LIST, hours);
+        startActivity(hourlyActivityIntent);
+    }
+
+    @OnClick(R.id.minutelyWeatherTextView)
+    public void MinutelyWeatherClick() {
+        Intent minutelyActivityIntent = new Intent(MainActivity.this, MinutelyWeatherActivity.class);
+        minutelyActivityIntent.putParcelableArrayListExtra(MINUTES_ARRAY_LIST, minutes);
+        startActivity(minutelyActivityIntent);
+    }
+
+    @OnClick(R.id.refreshWeatherTextView)
+    public void RefreshWeatherClick() {
+        getMyLocation();
+        Log.d(TAG, "LOCATION: " + mLatitude + " , " + mLongitude);
+    }
+
+    public void getWeather() {
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
 
@@ -79,8 +192,6 @@ public class MainActivity extends Activity {
         String units = "units=si";
 
         final String urlForeCast = foreCastURL + "/" + apiKey + "/" + mLatitude + "," + mLongitude + "?" + units;
-        //String url ="https://api.darksky.net/forecast/797573bdd40bc15d7f0536c8b663d042/37.8267,-122.4233?units=si";
-        //String url ="https://api.darksky.net/forecast/797573bdd40bc15d7f0536c8b663d0/37.8267,-122.4233?units=si"; //Error
 
         // Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, urlForeCast,
@@ -88,16 +199,24 @@ public class MainActivity extends Activity {
                     @Override
                     public void onResponse(String response) {
                         try {
-                            days = getDailyWeatherFromJson(response);
-                            hours = getHourlyWeaterFromJson(response);
-                            minutes = getMinutelyWeaterFromJson(response);
-
+                            Log.d(TAG, "URL: " + urlForeCast);
                             CurrentWeather currentWeather = getCurrentWeatherFromJson(response);
+
+                            days = getDailyWeatherFromJson(response);
+                            hours = getHourlyWeatherFromJson(response);
+                            minutes = getMinutelyWeatherFromJson(response);
+
+                            //for (Minute minute : minutes) {
+                            //    Log.d(TAG,minute.getTitle());
+                            //    Log.d(TAG,minute.getRainProbability());
+                            //}
+
                             iconImageView.setImageDrawable(currentWeather.getIconDrawableResource());
                             descriptionTextView.setText(currentWeather.getDescription());
                             currentTempTextView.setText(currentWeather.getCurrentTemperature());
                             highestTempTextView.setText(String.format("H: %s°", currentWeather.getHighestTemperature()));
                             lowestTempTextView.setText(String.format("L: %s°", currentWeather.getLowestTemperature()));
+
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -108,34 +227,14 @@ public class MainActivity extends Activity {
                 Toast.makeText(MainActivity.this, "Connection Error", Toast.LENGTH_LONG).show();
             }
         });
-
         // Add the request to the RequestQueue.
         queue.add(stringRequest);
+
     }
 
-    @OnClick(R.id.dailyWeatherTextView)
-    public void dailyWeatherClick(){
-        Intent dailyActivityIntent = new Intent(MainActivity.this, DailyWeatherActivity.class);
-        dailyActivityIntent.putExtra(DAYS_ARRAY_LIST, days);
-        startActivity(dailyActivityIntent);
-    }
-
-    @OnClick(R.id.hourlyWeatherTextView)
-    public void hourlyWeatherClick(){
-        Intent hourlyActivityIntent = new Intent(MainActivity.this, HourlyWeatherActivity.class);
-        hourlyActivityIntent.putExtra(HOURS_ARRAY_LIST,hours);
-        startActivity(hourlyActivityIntent);
-    }
-
-    @OnClick(R.id.minutelyWeatherTextView)
-    public void minutelyWeatherClick(){
-        Intent minutelyActivityIntent = new Intent(MainActivity.this, MinutelyWeatherActivity.class);
-        minutelyActivityIntent.putExtra(MINUTES_ARRAY_LIST, minutes);
-        startActivity(minutelyActivityIntent);
-    }
-
-    private CurrentWeather getCurrentWeatherFromJson(String json) throws JSONException{
+    private CurrentWeather getCurrentWeatherFromJson(String json) throws JSONException {
         JSONObject jsonObject = new JSONObject(json);
+
         JSONObject jsonWithCurrentWeather = jsonObject.getJSONObject(CURRENTLY);
         JSONObject jsonWithDailyWeather = jsonObject.getJSONObject(DAILY);
         JSONArray jsonWithDailyWeatherData = jsonWithDailyWeather.getJSONArray(DATA);
@@ -157,15 +256,16 @@ public class MainActivity extends Activity {
         return currentWeather;
     }
 
-    private ArrayList<Day> getDailyWeatherFromJson(String json) throws JSONException{
+    private ArrayList<Day> getDailyWeatherFromJson(String json) throws JSONException {
         DateFormat dateFormat = new SimpleDateFormat("EEEE");
         ArrayList<Day> days = new ArrayList<>();
         JSONObject jsonObject = new JSONObject(json);
-        JSONObject jsonWithDailyWeather = jsonObject.getJSONObject(DAILY);
-        JSONArray jsonWithDailyWeatherData = jsonWithDailyWeather.getJSONArray(DATA);
 
         String timezone = jsonObject.getString(TIMEZONE);
         dateFormat.setTimeZone(TimeZone.getTimeZone(timezone));
+
+        JSONObject jsonWithDailyWeather = jsonObject.getJSONObject(DAILY);
+        JSONArray jsonWithDailyWeatherData = jsonWithDailyWeather.getJSONArray(DATA);
 
         for (int i = 0; i < jsonWithDailyWeatherData.length(); i++) {
             Day day = new Day();
@@ -184,7 +284,7 @@ public class MainActivity extends Activity {
         return days;
     }
 
-    public ArrayList<Hour> getHourlyWeaterFromJson(String json) throws JSONException {
+    public ArrayList<Hour> getHourlyWeatherFromJson(String json) throws JSONException {
         DateFormat dateFormat = new SimpleDateFormat("HH:mm");
         ArrayList<Hour> hours = new ArrayList<>();
         JSONObject jsonObject = new JSONObject(json);
@@ -207,31 +307,66 @@ public class MainActivity extends Activity {
 
             hours.add(hour);
         }
-
         return hours;
     }
-    public ArrayList<Minute> getMinutelyWeaterFromJson(String json) throws JSONException {
-        DateFormat dateFormat = new SimpleDateFormat("HH:mm");
-        ArrayList<Minute> minutes = new ArrayList<>();
-        JSONObject jsonObject = new JSONObject(json);
 
-        String timezone = jsonObject.getString(TIMEZONE);
-        dateFormat.setTimeZone(TimeZone.getTimeZone(timezone));
+    public ArrayList<Minute> getMinutelyWeatherFromJson(String json) throws JSONException {
 
-        JSONObject jsonWithMinutelyWeather = jsonObject.getJSONObject(MINUTELY);
-        JSONArray jsonWithMinutelyWeatherData = jsonWithMinutelyWeather.getJSONArray(DATA);
-
-        for (int i = 0; i < jsonWithMinutelyWeatherData.length(); i++) {
-            Minute minute = new Minute();
-            JSONObject jsonWithMinuteData = jsonWithMinutelyWeatherData.getJSONObject(i);
-
-            String title = dateFormat.format(jsonWithMinuteData.getDouble(TIME) * 1000);
-            String precipProbability = "Rain Probability: " + jsonWithMinuteData.getDouble(PRECIP_PROBABILITY) * 100 + "%";
-
-            minute.setTitle(title);
-            minute.setRainProbability(precipProbability);
-            minutes.add(minute);
+        if(json == null){
+            return null;
         }
-        return minutes;
+
+        else{
+            DateFormat dateFormat = new SimpleDateFormat("HH:mm");
+            ArrayList<Minute> minutes = new ArrayList<>();
+            JSONObject jsonObject = new JSONObject(json);
+
+            String timezone = jsonObject.getString(TIMEZONE);
+            dateFormat.setTimeZone(TimeZone.getTimeZone(timezone));
+
+            if(jsonObject.isNull(MINUTELY)){
+                return null;
+            }
+            JSONObject jsonWithMinutelyWeather = jsonObject.getJSONObject(MINUTELY);
+            JSONArray jsonWithMinutelyWeatherData = jsonWithMinutelyWeather.getJSONArray(DATA);
+
+            for (int i = 0; i < jsonWithMinutelyWeatherData.length(); i++) {
+                Minute minute = new Minute();
+                JSONObject jsonWithMinuteData = jsonWithMinutelyWeatherData.getJSONObject(i);
+
+                String title = dateFormat.format(jsonWithMinuteData.getDouble(TIME) * 1000);
+                String precipProbability = "Rain Probability: " + jsonWithMinuteData.getDouble(PRECIP_PROBABILITY) * 100 + "%";
+
+                minute.setTitle(title);
+                minute.setRainProbability(precipProbability);
+                minutes.add(minute);
+            }
+            return minutes;
+        }
     }
+
+    private void getMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            if (mLastLocation != null) {
+                /*
+                Toast.makeText(MainActivity.this,"Location: " +
+                                String.valueOf(mLastLocation.getLatitude()) + ", "
+                                + String.valueOf(mLastLocation.getLongitude()),
+                        Toast.LENGTH_LONG).show();
+                            */
+                mLatitude = String.valueOf(mLastLocation.getLatitude());
+                mLongitude = String.valueOf(mLastLocation.getLongitude());
+
+                Log.d("DEBUG", "current location: " + String.valueOf(mLastLocation.getLatitude())
+                        + ", " + String.valueOf(mLastLocation.getLongitude()));
+                getWeather();
+
+            } else {
+                Toast.makeText(MainActivity.this, "Need your permission!", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
 }
